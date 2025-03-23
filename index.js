18c5877a6770c78bb37571358cb7bd528dfaf1da
Vishnu25112003 <vishnumurugavelu@gmail.com>
2025-03-23 19:10:12 +0530

    Notification Changes

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ document.querySelector(".shortcut-btn").onclick=function(){
     document.querySelector(".nav-halfscreen").classList.toggle("hide");
 }
 
-let popup=document.getElementById("popup");
-function openPopup(){
-    popup.classList.add("open-popup");
+let popup_buy=document.getElementById("popup-buy");
+function openPopup_buy() {
+    popup_buy.style.visibility = "visible";
+    popup_buy.style.transform = "translate(-50%, -50%) scale(1)";
 }
-function closePopup(){
-    popup.classList.remove("open-popup");
+function closePopup_buy() {
+    setTimeout(() => {
+        popup_buy.style.visibility = "hidden";
+    }, 100); 
+}
+let popup_cart=document.getElementById("popup-cart");
+function openPopup_cart() {
+    popup_cart.style.visibility = "visible";
+    popup_cart.style.transform = "translate(-50%, -50%) scale(1)";
+}
+function closePopup_cart() {
+    setTimeout(() => {
+        popup_cart.style.visibility = "hidden";
+    }, 100); 
 }
